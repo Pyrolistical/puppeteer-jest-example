cafa9769469d376b73bc4d0f49eb25dad6716b15
Ronald Chen <ronald@battlefy.com>
2018-02-05 15:47:33 -0800

    promisfied and ensure cleanup of browser and files

--- index.js
+++ index.js
@@ -3,23 +3,53 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-const JestEnvironmentFactory = require('./jest-environment-factory')
+const puppeteerEnvironmentFactory = require('./puppeteer-environment-factory')
+
+function promiseCallback(closure) {
+  return new Promise((resolve, reject) => {
+    closure((error, ...results) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(results)
+    })
+  })
+}
+
+const writeFile = (file, data) => promiseCallback((callback) => fs.writeFile(file, data, callback))
+const unlink = (file) => promiseCallback((callback) => fs.unlink(file, callback))
+const exec = (command) => promiseCallback((callback) => child_process.exec(command, callback))
 
 async function main() {
   const browser = await puppeteer.launch({})
+  const cleanupBrowser = () => {
+    console.log('closing')
+    return browser.close()
+  }
 
-  const jestEnvironmentFile = path.join(__dirname, 'jest-environment.js')
-  fs.writeFileSync(jestEnvironmentFile, JestEnvironmentFactory(browser.wsEndpoint()))
-
-  child_process.exec('node_modules/.bin/jest', async (error) => {
-    if (error) {
-      console.log(error)
-      // fall through to finally
+  try {
+    const puppeteerEnvironmentFile = path.join(__dirname, 'puppeteer-environment.js')
+    const cleanupPuppeteerEnvironmentFile = () => {
+      return unlink(puppeteerEnvironmentFile)
     }
-    // finally
-    await browser.close()
-    fs.unlinkSync(jestEnvironmentFile)
-  })
+
+    try {
+      await writeFile(puppeteerEnvironmentFile, puppeteerEnvironmentFactory(browser.wsEndpoint()))
+
+      const [stdout, stderr] = await exec('node_modules/.bin/jest')
+      console.log({stdout, stderr})
+    } catch (error) {
+      await cleanupPuppeteerEnvironmentFile()
+      throw error
+    }
+    await cleanupPuppeteerEnvironmentFile()
+
+  } catch (error) {
+    await cleanupBrowser()
+    throw error
+  }
+  await cleanupBrowser()
+
 }
 
 main()
